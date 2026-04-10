a814481858beab727f448a8b00f2a40f9dc3add4
stds58 <stds58@gmail.com>
2026-04-11 01:16:41 +0300

    feat: infrastructure

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Определяем список файлов массивом для читаемости
 COMPOSE_FILES=(
     "docker-compose.yml"
     "docker-compose-kafka.yml"
@@ -14,12 +13,19 @@ COMPOSE_FILES=(
     "docker-compose-wrk.yml"
 )
 
+if [ "$1" == "dev" ]; then
+    echo "Running in DEV mode with code mounts..."
+    COMPOSE_FILES+=("docker-compose-app.dev.yml")
+    shift # Удаляем 'dev' из аргументов
+else
+    echo "Running in PROD/STAGE mode (built image, no mounts)..."
+    COMPOSE_FILES+=("docker-compose-app.yml")
+fi
+
 # Собираем строку через разделитель ':'
 COMPOSE_FILE_STRING=$(IFS=:; echo "${COMPOSE_FILES[*]}")
 
-# Экспортируем переменные
 export COMPOSE_PATH_SEPARATOR=:
 export COMPOSE_FILE="$COMPOSE_FILE_STRING"
 
-# Запускаем docker compose с переданными аргументами
-docker compose "$@"
+"$@"
